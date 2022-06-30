19374890d81fc95c80597bdafe6e5db1e78e77e9
Your Name <you@example.com>
2022-07-01 04:15:08 +1000

    add note and delete note function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,52 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+
 
 function App() {
- return (  
-          <div>
-            <Header />
-            <Note />
-            <Footer />
-          </div>
-        );
+
+  const[notes, setNotes] = React.useState([]);
+
+  function addNote(newNote){
+    setNotes(prevNotes => {
+      return [...prevNotes,newNote];
+    });  
+  }
+
+  function deleteNote(id){
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+
+  return (
+    <div>
+      <Header />
+      <CreateArea 
+      onAdd={addNote}
+      />
+      {notes.map((noteItem,index) => 
+      {
+          return (
+          <Note
+          key={index}
+          id={index} 
+          title={noteItem.title} 
+          content={noteItem.content}
+          onDelete={deleteNote}
+          />
+          );
+
+      })}
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
-function Note() {
+function Note(props) {
+
+    function handleClick(){
+        props.onDelete(props.id);
+    }
+
+
+
+
     return (
         <div className="note">
-            <h1>title</h1>
-            <p>content</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+            <button onClick={handleClick}>DELETE</button>
         </div>
     );
 }
